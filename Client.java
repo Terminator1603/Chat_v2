29d3402faa0841893888d9b7c57cc79fb5117320
Terminator1603 <31101072+Terminator1603@users.noreply.github.com>
2018-07-14 00:15:49 +0530

    Added clear and exit functions
    Also an emoji. A lot of potential here

--- Client.java
+++ Client.java
@@ -1,18 +1,16 @@
 import java.net.*;
 import java.io.*;
+ 
 public class Client {
-
+ 
     public static void main(String[] args) {
-        String clientID;
-        if(args.length==0){
-            clientID = idGenerator(3);
-        }else{
-            clientID = args[0];
-        }
-        String hostname = "localhost";
-        int port = 6868;
+        if (args.length < 2) return;
+ 
+        String hostname = args[0];
+        int port = Integer.parseInt(args[1]);
+        String clientID = args[2];
         try (Socket socket = new Socket(hostname, port)) {
-            System.out.println(clientID);
+ 
             OutputStream output = socket.getOutputStream();
             PrintWriter writer = new PrintWriter(output, true);
             writer.println(clientID);
@@ -23,30 +21,45 @@ public class Client {
             MessageReceive mr = new MessageReceive(reader);
             mr.start();
             do {
-                text = console.readLine();
-
+                text = console.readLine("Enter text: ");
+                try{
+                    checkFunc(text,socket);
+                }catch(Exception e){}
                 writer.println(text);
-
                 //String message = reader.readLine();
+ 
                 //System.out.println(message);
+ 
             } while (!text.equals("bye"));
-
+ 
             socket.close();
-
+ 
         } catch (UnknownHostException ex) {
-
+ 
             System.out.println("Server not found: " + ex.getMessage());
-
-        } catch (Exception ex) {
-
+ 
+        } catch (IOException ex) {
+ 
             System.out.println("I/O error: " + ex.getMessage());
         }
     }
-    public static String idGenerator(int n){
-        String id = "";
-        for(int i =0;i<n;i++){
-            id += (char)(int)(Math.random()*(91-65)+65);
+    static void checkFunc(String text, Socket s) throws Exception{
+        switch(text){
+            case "/c":{
+                System.out.print('\u000C');
+                System.out.println("Enter a number");
+                break;
+            }
+            case "/e":{
+                System.out.print('\u000C');
+                s.close();
+                System.exit(0);
+                break;
+            }
+            case "/wot":{
+                System.out.println("(ಠ_ಠ)");
+                break;
+            }
         }
-        return id;
     }
 }
